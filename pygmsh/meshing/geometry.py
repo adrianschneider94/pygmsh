cc6d9cbe5e807291581dd90589616061764560bb
adrianschneider94 <post@adrian-schneider.de>
2018-07-04 14:19:07 +0200

    Removed bug (local variable fields < - > imported module fields)

--- pygmsh/meshing/geometry.py
+++ pygmsh/meshing/geometry.py
@@ -1,7 +1,7 @@
 from ..built_in.geometry import Geometry as BaseBuiltInGeometry
 from ..opencascade.geometry import Geometry as BaseOpenCascadeGeometry
 
-from . import fields
+from . import fields as Fields
 
 
 class MeshingMixIn(object):
@@ -20,22 +20,22 @@ class MeshingMixIn(object):
         return True
 
     def add_distance_field(self, *, objects, n_nodes_by_edge=20):
-        field = fields.Distance(objects=objects, n_nodes_by_edge=n_nodes_by_edge)
+        field = Fields.Distance(objects=objects, n_nodes_by_edge=n_nodes_by_edge)
         self.add_field(field)
         return field
 
     def add_math_eval_field(self, *, expression, fields=None):
-        field = fields.MathEval(expression=expression, fields=fields)
+        field = Fields.MathEval(expression=expression, fields=fields)
         self.add_field(field)
         return field
 
     def add_min_field(self, *, fields):
-        field = fields.Min(fields=fields)
+        field = Fields.Min(fields=fields)
         self.add_field(field)
         return field
 
     def add_max_field(self, *, fields):
-        field = fields.Max(fields=fields)
+        field = Fields.Max(fields=fields)
         self.add_field(field)
         return field
 
